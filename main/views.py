decfe40a103925bb6cf32e985675c54885c3e0d4
you <you@example.com>
2020-03-19 00:35:42 +0700

    before collapse

--- main/views.py
+++ main/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
 
 def index(request):
     return render(request,'main/index.html')
 
+
 def login(request):
-    return render(request,'main/login.html')
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        # Redirect to a success page.
+        ...
+    else:
+        # Return an 'invalid login' error message.
+        ...
