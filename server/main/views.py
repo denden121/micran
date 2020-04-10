70cdf7cf74d0d9e998237044da516735875f27f8
yura <ledovskij.yura@gmail.com>
2020-04-10 03:59:35 -0400

    Fixed authorization

--- server/main/views.py
+++ server/main/views.py
@@ -1,19 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
+from django.views.decorators.csrf import csrf_exempt
+from django.middleware.csrf import get_token
 
 def index(request):
     return render(request,'main/index.html')
 
-
+@csrf_exempt
 def login(request):
     username = request.POST['username']
     password = request.POST['password']
-    user = authenticate(request, username=username, password=password)
+    csrf_token = get_token(request)
+    print(password)
+    user = authenticate(username=username, password=password)
     if user is not None:
-        login(request, user)
-        # Redirect to a success page.
-        ...
+        return HttpResponse('success')
     else:
-        # Return an 'invalid login' error message.
-        ...
+        return HttpResponse('fail')
