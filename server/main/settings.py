85c56302a018dc6c69e7f0f098cec86b119a0be7
Alex <alex0lihach@gmail.com>
2020-04-18 16:02:54 +0700

    Fix postgres

--- server/main/settings.py
+++ server/main/settings.py
@@ -61,6 +61,7 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -71,6 +72,7 @@ DATABASES = {
         'PORT' : 5432,
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
