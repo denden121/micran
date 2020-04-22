7f4223a8725aa406b443223a6d3549f789647b01
den <lisnik.opros@gmail.com>
2020-04-22 04:24:11 -0500

    сделал первую форму

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,19 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
-// import Auth from './Auth/Auth'
-// import axios from 'axios'
-class App extends Component {
-    // state = {
-    //     date: []
-    // }
-    // authHandler=()=>{
-    //     axios.post('http://127.0.0.1:8000/login/',{'username':'first','password':'w8$jdP#u7LMFRJn'})
-    //         .then(response => {
-    //             this.state.date = response
-    //         })
-    // }
+import Auth from './Auth/Auth'
+import axios from 'axios'
+class App extends Component{
+    authHandler(){
+        axios.post('http://127.0.0.1:8000/login/',{'username':'first','password':'w8$jdP#u7LMFRJn'})
+            .then(response => {
+                console.log(response)
+            })
+    }
     render() {
-        return ( <
-            // h1 > Hello world < /h1>
+        return (
+            <div className="App">
+                <Auth authHandler={this.authHandler}/>
+            </div>
         );
     }
 }
