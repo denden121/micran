d3764b3900f029bae74e5a66307436223ba82fa8
den <lisnik.opros@gmail.com>
2020-04-22 01:24:38 -0500

    добал зависимости

--- server/main/settings.py
+++ server/main/settings.py
@@ -63,16 +63,16 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'report',
-        'USER' : 'micran',
-        'PASSWORD' : '12345678z',
-        'HOST' : 'db2',
-        'PORT' : 5432,
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'report',
+#         'USER' : 'micran',
+#         'PASSWORD' : '12345678z',
+#         'HOST' : 'db2',
+#         'PORT' : 5432,
+#     }
+# }
 
 
 # Password validation
