81eb88b17ad8644c3814fbfc1da27ac3b1df031b
yura <ledovskij.yura@gmail.com>
2020-04-22 04:18:53 -0400

    Fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,20 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import Auth from './Auth/Auth'
-import axios from 'axios'
-class App extends Component{
-    state = {
-        email:'first',
-        password:'w8$jdP#u7LMFRJn'
-    }
-    authHandler(){
-        axios.post('http://127.0.0.1:8000/login/',{'username':'first','password':'w8$jdP#u7LMFRJn'})
-            .then(response => {
-                console.log(response)
-            })
-    }
+// import Auth from './Auth/Auth'
+// import axios from 'axios'
+class App extends Component {
+    // state = {
+    //     date: []
+    // }
+    // authHandler=()=>{
+    //     axios.post('http://127.0.0.1:8000/login/',{'username':'first','password':'w8$jdP#u7LMFRJn'})
+    //         .then(response => {
+    //             this.state.date = response
+    //         })
+    // }
     render() {
-        return (
-            <div className="App">
-                <Auth authHandler={this.authHandler}/>
-
-            </div>
+        return ( <
+            // h1 > Hello world < /h1>
         );
     }
 }
