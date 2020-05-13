29024df705722f19d6dba7e1dfd631d8f78f295c
Anastasia <nastyaoseeva@gmail.com>
2020-05-13 23:12:48 +0700

    Изменила стили Report и добавила компонент lookRport

---
+++ client/src/homePage/lookReport/lookReport.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import "./lookReport.css"
+
+class LookReport extends Component{
+   render(){
+       return(
+           <div>
+
+           </div>
+       )
+   }
+}
+
+export default LookReport
