ba9428868cba507710e6f9e7ff39a81f20e95210
Anastasia <nastyaoseeva@gmail.com>
2020-06-17 13:31:55 +0700

    PersonData

--- client/src/ Components/ PersonCabinet/MainPage/Main.js
+++ client/src/ Components/ PersonCabinet/MainPage/Main.js
@@ -3,13 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from "../Navigation/Navigation";
 import Header from "../Header/Header"
 import SendReport from "../SendReport/SendReport"
+import PersonData from "../PersonData/PersonData"
 
 const Main =(props)=>{
     return(
         <div className="container">
             <Header/>
-            <Navigation/>
-            <SendReport/>
+            {/*<Navigation/>*/}
+            <PersonData/>
         </div>
     )
 }
