e2db1fb5d110d55601761cd50b62639c4d11b758
Yura <ledovskij.yura@gmail.com>
2020-06-26 12:50:32 +0700

    Docker ready

--- server/main/settings.py
+++ server/main/settings.py
@@ -58,7 +58,7 @@ AUTHENTICATION_BACKENDS = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','0.0.0.0']
 LOGIN_REDIRECT_URL = '/admin'
 LOGIN_URL = '/login'
 
