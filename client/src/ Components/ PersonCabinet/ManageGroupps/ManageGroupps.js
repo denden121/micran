696a733475564d446cc54c2a9b1f4858173169ae
den <lisnik.20@yandex.ru>
2020-07-02 11:45:38 +0700

    finish request for table

--- client/src/ Components/ PersonCabinet/ManageGroupps/ManageGroupps.js
+++ client/src/ Components/ PersonCabinet/ManageGroupps/ManageGroupps.js
@@ -1,8 +1,8 @@
 import React, {Component} from "react"
 import NameGroupps from "./NameGroupps/NameGroupps"
 
-class ManageGroupps extends React.Component{
-    componentDidMount() {
+class ManageGroups extends React.Component{
+    async componentDidMount() {
         let token = localStorage.getItem('token')
         let myHeaders = new Headers();
         myHeaders.append("Authorization", token);
@@ -13,15 +13,17 @@ class ManageGroupps extends React.Component{
             redirect: 'follow'
         };
 
-        fetch("http://127.0.0.1:8000/groups_admin/", requestOptions)
-            // .then(response => console.log(this.setState({groupps:response.json()})))
-            .then(response => response.json())
-            .then(result =>console.log(result))
+        await fetch("http://127.0.0.1:8000/groups_admin/", requestOptions)
+            // .then(response => console.log(this.setState({groups: response.json()})))
+            .then(response => console.log(response.text()))
+            .then(result => console.log(result))
+        let temp = Array.from(this.state.groups)
+        console.log('array', temp)
 
     }
 
     state = {
-        groupps:{}
+        groups:{}
     }
     render(){
         return(
@@ -35,7 +37,7 @@ class ManageGroupps extends React.Component{
                 </tr>
                 </thead>
                 <tbody>
-                <NameGroupps listGroup = {this.state.groupps}/>
+                    <NameGroupps listGroup = {this.state.groups}/>
                 </tbody>
             </table>
 
@@ -44,4 +46,4 @@ class ManageGroupps extends React.Component{
         )
     }
 }
-export default ManageGroupps;
+export default ManageGroups;
