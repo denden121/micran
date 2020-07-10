38eff3cb110d9408d320d26b3292ea8475599be8
Yura <ledovskij.yura@gmail.com>
2020-07-10 11:55:32 +0700

    FFF

--- client/src/ Components/ PersonCabinet/Navigation/Calendar/Calendar.js
+++ client/src/ Components/ PersonCabinet/Navigation/Calendar/Calendar.js
@@ -5,9 +5,9 @@ const Calendar =(props)=>{
     return(
         <div className="month-picker">
             <div className="nativeDatePicker">   
-             <input min="2015" type="month" id="month-visit" name="month-visit" className="form-control"/>
+             <input min="2015" type="month" id="month-visit" defaultValue={'December 2020' } name="month-visit" className="form-control"/>
                 <span className="validity"></span>
-            </div>
+            </div>      
             </div>
     )
 }
