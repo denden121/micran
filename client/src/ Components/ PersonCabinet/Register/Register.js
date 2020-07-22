2a0e33fb1f00ea08d12faf1215d918ccbc90e8d5
Anastasia <nastya_oseeva@mail.ru>
2020-07-22 18:25:45 +0700

    Объединение проектов

--- client/src/ Components/ PersonCabinet/Register/Register.js
+++ client/src/ Components/ PersonCabinet/Register/Register.js
@@ -1,6 +1,6 @@
 import React from "react"
 import RegisterTable from "./RegisterTable/RegisterTable"
-import {Redirect} from 'react-router-dom'
+
 
 class Register extends React.Component{
     onClickNewProject = () =>{
@@ -9,6 +9,9 @@ class Register extends React.Component{
     }
     onClickBack= () =>{
         document.location='/cabinet/admin/register'
+    }
+    onClickUnit= () =>{
+        document.location='/cabinet/admin/unit_projects'
     }
     render(){
         return(
@@ -19,7 +22,7 @@ class Register extends React.Component{
                         <h3 className="text-left">Реестр проектов</h3>
                         <div className="buttons text-left">
                             <button type="button" className="btn btn-success btn-sm" onClick={this.onClickNewProject}>Добавить новый</button>
-                            <button type="button" className="btn btn-danger btn-sm" >Объединить</button>
+                            <button type="button" className="btn btn-danger btn-sm" onClick={this.onClickUnit}>Объединить</button>
                             <button type="button" className="btn btn-primary btn-sm" >Экспорт результатов</button>
                         </div>
                         <hr className="normal"/>
