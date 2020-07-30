4a971e54e7594ab7770d59cdd3cfb01f4a79dd05
den <lisnik.20@yandex.ru>
2020-07-30 17:35:43 +0700

    доделал баги с отчетами

--- client/src/ Components/ PersonCabinet/Register/RegisterTable/RegisterTable.js
+++ client/src/ Components/ PersonCabinet/Register/RegisterTable/RegisterTable.js
@@ -1,7 +1,29 @@
 import React from "react"
 import "./RegisterTable.css"
 
-const RegisterTable =(props)=>{
+const ListProjects = (props) => {
+    let result = Array.from(props.projects)
+    return result = result.map((project,index) =>{
+        return(
+            <tr>
+                <td>{project.fields.direction}</td>
+                <td>{project.fields.name}</td>
+                <td>{project.fields.manager}</td>
+                <td>{project.fields.chief_designer}</td>
+                <td>{project.fields.deputy_chief_designer}</td>
+                <td>{project.fields.production_order}</td>
+                <td>{project.fields.contract}</td>
+                <td>{project.fields.client}</td>
+                <td>{project.fields.type}</td>
+                <td>{project.fields.status}</td>
+                <td>{project.fields.report_availability}</td>
+                <td>{project.fields.acceptance_vp?'да':'нет*'}</td>
+            </tr>
+        )
+    })
+}
+
+const RegisterTable = (props) =>{
     return(
         <div className="container-fluid">
             <div className="row">
@@ -26,7 +48,7 @@ const RegisterTable =(props)=>{
                                 </tr>
                             </thead>
                             <tbody>
-                                <td></td>
+                                <ListProjects projects = {props.projects}/>
                             </tbody>
                         </table>
                     </div>
