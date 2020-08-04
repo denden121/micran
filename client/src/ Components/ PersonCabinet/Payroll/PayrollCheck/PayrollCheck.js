12e8a864e1c15d66c61fb534b10ab70327ffdf63
den <lisnik.20@yandex.ru>
2020-08-04 20:37:40 +0700

    доделыл фильтры на зп

--- client/src/ Components/ PersonCabinet/Payroll/PayrollCheck/PayrollCheck.js
+++ client/src/ Components/ PersonCabinet/Payroll/PayrollCheck/PayrollCheck.js
@@ -4,29 +4,29 @@ import "../Payroll.css"
 const PayrollCheck =(props)=>{
     return(
         <div>
-        <div className="form-check text-left">
+        <div onChange={props.onChangeFilter} className="form-check text-left">
             <label className="checkbox">
-                <input type="checkbox" className="form-check-input"/>
+                <input value={'hide-salary'} type="checkbox" className="form-check-input"/>
                     Скрыть колонки с результатами расчетов
             </label>
             <br/>
             <label className="checkbox">
-                <input type="checkbox" className="form-check-input"/>
+                <input value={'hide-norm-time'} type="checkbox" className="form-check-input"/>
                     Скрыть нормы часов
             </label>
             <br/>
             <label className="checkbox">
-                <input type="checkbox" className="form-check-input"/>
+                <input value={'hide-zero-report'} type="checkbox" className="form-check-input"/>
                     Скрыть сотрудников с "нулевыми" отчетами
             </label>
             <br/>
             <label className="checkbox">
-                <input type="checkbox" className="form-check-input"/>
+                <input value={'hide-technician'} type="checkbox" className="form-check-input"/>
                     Скрыть лаборантов
             </label>
             <br/>
             <label className="checkbox">
-                <input type="checkbox" className="form-check-input"/>
+                <input value={'hide-another-people'} type="checkbox" className="form-check-input"/>
                     Показывать только сотрудников НИИ СЭС
             </label>
         </div>
