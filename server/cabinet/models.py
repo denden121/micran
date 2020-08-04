80fdf3a19995d5c914a115a297d9cf8047e3b20b
Yura <ledovskij.yura@gmail.com>
2020-08-04 12:47:58 +0700

    Fix

--- server/cabinet/models.py
+++ server/cabinet/models.py
@@ -51,7 +51,6 @@ class Profile(models.Model):
     shift = models.CharField(max_length=30, blank=True)
     part_time_job = models.CharField(max_length=30, blank=True)
     lateness = models.CharField(max_length=30, blank=True)
-    # first_time = models.BooleanField(default=False)
 
     def __str__(self):
         return self.user.username
