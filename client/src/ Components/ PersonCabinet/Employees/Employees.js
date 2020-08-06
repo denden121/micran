b92e9bf64a6d6416d4d6f8f3fa75be512a514b6a
Anastasia <nastya_oseeva@mail.ru>
2020-08-06 12:43:03 +0700

    карточка сотрудника

--- client/src/ Components/ PersonCabinet/Employees/Employees.js
+++ client/src/ Components/ PersonCabinet/Employees/Employees.js
@@ -1,7 +1,14 @@
 import React from "react"
 import ListEmp from "./ListEmp/ListEmp"
+import "./ListEmp/CardModal/CardModal.css"
+import CardModal from "./ListEmp/CardModal/CardModal"
+import "./Employees.css"
 
 class Employees extends React.Component{
+    onClickPerson = ()=>{
+        document.querySelector('.background-modal').style.display = 'flex'
+        console.log('person',document.querySelector('.background-modal').style.display)
+    }
     render(){
         return(
             <div className="container-fluid">
@@ -9,8 +16,18 @@ class Employees extends React.Component{
                 <div className="row">
                     <div className="col-md-12 col-lg-12">
                         
-                        <ListEmp/>
+                        <ListEmp onClickPerson={this.onClickPerson}/>
                     </div>
+                </div>
+                <div className="background-modal">
+                    <div className="modal">
+                        <CardModal/>
+                    </div>
+                </div>
+                <div className="background-modal">
+                    
+                        <CardModal/>
+                    
                 </div>
             </div>
         )
--- client/src/ Components/ PersonCabinet/Employees/ListEmp/ListEmp.js
+++ client/src/ Components/ PersonCabinet/Employees/ListEmp/ListEmp.js
@@ -1,23 +1,37 @@
 import React from "react"
+import CardModal from "./CardModal/CardModal"
 
 const ListEmp =(props)=>{
     return(
         <div className="row">
             <div className="col-sm-12 col-md-12 col-lg-12">
-                <table className="table table-bordered">
-                    <thead>
-                        <tr>
-                            <th scope="col">№ БД</th>
-                            <th scope="col">№ 1С</th>
-                            <th scope="col">ФИО</th>
-                            <th scope="col">Дата труд-ва</th>
-                            <th scope="col">Смена</th>
-                            <th scope="col">НИИСЭС</th>
-                            <th scope="col">Окладная СОТ</th>
-                            <th scope="col">Группы</th>
-                            <th scope="col">Подразделения, должность</th>
-                        </tr>
-                    </thead>
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                    <th scope="col">№ БД</th>
+                    <th scope="col">№ 1С</th>
+                    <th scope="col">ФИО</th>
+                    <th scope="col">Дата труд-ва</th>
+                    <th scope="col">Смена</th>
+                    <th scope="col">НИИСЭС</th>
+                    <th scope="col">Окладная СОТ</th>
+                    <th scope="col">Группы</th>
+                    <th scope="col">Подразделения,должность</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <th scope="row">1</th>
+                    <td>1</td>
+                    <td><a onClick={props.onClickPerson} style={{cursor:"pointer"}}>Осеева Анастасия Михайловна</a></td>
+                    <td>12.02.2020</td>
+                    <td>папрпа</td>
+                    <td>иапиа</td>
+                    <td>пвдлпо</td>
+                    <td>пвапвп</td>
+                    <td>пиаиапи</td>
+                    </tr>                                       
+                </tbody>
                 </table>
             </div>
         </div>
