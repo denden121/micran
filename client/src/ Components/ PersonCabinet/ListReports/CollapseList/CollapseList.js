46274512f4bc6d21f09eecb65662473d0b3d619f
den <lisnik.20@yandex.ru>
2020-09-09 21:53:26 +0700

    f

--- client/src/ Components/ PersonCabinet/ListReports/CollapseList/CollapseList.js
+++ client/src/ Components/ PersonCabinet/ListReports/CollapseList/CollapseList.js
@@ -5,7 +5,7 @@ import { Select } from 'antd';
 const plainOptions = ['Весь список', 'Только НИИ СЭС', 'Отображать заблокированный отчет в виде таблицы'];
 const { Option } = Select;
 
-const CollapseList =()=>{
+const CollapseList =(props)=>{
     return(
         <div className="container-fluid">
             <div className="row">
@@ -17,14 +17,17 @@ const CollapseList =()=>{
             <div className="row">
                 <div className="col-lg-12">
                 <Select
+                    options={props.departments}
                     placeholder="Выбрать департамент"
                     style={{width:"70%"}}
+                    onChange={props.onChangeSelectDepartments}
                 />
                 </div>
                 <br/>
                 <br/>
                 <div className="col-lg-12">
                 <Select
+                    options={props.subdepartments}
                     placeholder="Все доступные подразделения"
                     style={{width:"70%"}}
                 />
--- client/src/ Components/ PersonCabinet/ListReports/ListReports.js
+++ client/src/ Components/ PersonCabinet/ListReports/ListReports.js
@@ -5,6 +5,55 @@ import ReportsTable from "./ReportsTable/ReportsTable"
 
 
 class ListReports extends React.Component {
+    state = {
+        departments:[],
+        select_department:'',
+        subdepartments:[],
+        select_subdepartments:'',
+        reports:[],
+    }
+    componentDidMount() {
+        this.loadDepartments();
+    }
+    loadDepartments=()=>{
+        let token = localStorage.getItem('token')
+        let myHeaders = new Headers()
+        myHeaders.append("Authorization", token)
+        let requestOptions = {
+            method: 'GET',
+            headers: myHeaders,
+            redirect: 'follow'
+        }
+        fetch("http://127.0.0.1:8000/departments/simple/", requestOptions)
+            .then(response =>  response.json())
+            .then(result => {
+                let actions = Array.from(result).map((department)=>{
+                    // console.log(department)
+                    return {value:`${department.pk}`,label:`${department.fields.code +' '+ department.fields.name}`}
+                })
+                this.setState({departments: actions})})
+            .catch(error => console.log('error', error))
+    }
+    onChangeSelectDepartments=(e)=>{
+        let token = localStorage.getItem('token')
+        let myHeaders = new Headers()
+        myHeaders.append("Authorization", token)
+        let requestOptions = {
+            method: 'GET',
+            headers: myHeaders,
+            redirect: 'follow'
+        }
+        fetch(`http://127.0.0.1:8000/departments/${e}/subdepartments/`, requestOptions)
+            .then(response =>  response.json())
+            .then(result => {
+                console.log('sub',result)
+                let subdepartments = Array.from(result).map((subdepartment)=>{
+                    // console.log(subdepartment)
+                    return {value:`${subdepartment.pk}`,label:`${subdepartment.fields.code +' '+ subdepartment.fields.name}`}
+                })
+                this.setState({subdepartments: subdepartments})})
+            .catch(error => console.log('error', error))
+    }
     render(){
         const { Panel } = Collapse;
         function callback(key) {
@@ -18,7 +67,10 @@ class ListReports extends React.Component {
                     <hr/>
                     <Collapse accordion defaultActiveKey={['1']} onChange={callback}>
                     <Panel header="Параметры отображения" className="text-left" key="1">
-                        <CollapseList/>
+                        <CollapseList
+                            onChangeSelectDepartments = {this.onChangeSelectDepartments}
+                            departments = {this.state.departments}
+                            subdepartments = {this.state.subdepartments}/>
                     </Panel>
                     </Collapse>    
                     </div>
