926282581544f06b71b18fc7312f1ac4ef25b7fb
Anastasia <nastya_oseeva@mail.ru>
2020-09-21 13:32:43 +0700

    Модалка

--- client/src/ Components/ PersonCabinet/Administration/AddGroups/EditGroupps/EditGroupps.js
+++ client/src/ Components/ PersonCabinet/Administration/AddGroups/EditGroupps/EditGroupps.js
@@ -10,6 +10,7 @@ import {
     InputNumber,
     TreeSelect,
     Switch,
+    Modal
   } from 'antd';
 
 const { TextArea } = Input;
@@ -38,11 +39,40 @@ class  EditGroups extends React.Component{
         this.loadDates()
     }
 
+    showModal = () => {
+        this.setState({
+          visible: true,
+        });
+      };
+    
+      handleOk = e => {
+        console.log(e);
+        this.setState({
+          visible: false,
+        });
+      };
+    
+      handleCancel = e => {
+        console.log(e);
+        this.setState({
+          visible: false,
+        });
+      };
+    
     render() {
 
         return (
+            
             <div className="container-fluid">
-                <div className="row">
+
+                <Modal
+                title="Редактирование групп"
+                visible={this.state.visible}
+                onOk={this.handleOk}
+                onCancel={this.handleCancel}
+                width={900}
+                >
+                    <div className="row">
                     <div className=" col-lg-12">
                         <Card>
                             <div className="row">
@@ -88,6 +118,11 @@ class  EditGroups extends React.Component{
                         </Card>
                     </div>
                 </div>
+                </Modal>
+                <Button type="primary" onClick={this.showModal}>
+                    Редактирование групп
+                </Button>
+                
             </div>
         )
     }
