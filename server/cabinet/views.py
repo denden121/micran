ff322a55330a75f10064332e660e56663ebfbaf8
Yura <ledovskij.yura@gmail.com>
2020-09-23 17:04:04 +0700

    Check this out

--- server/cabinet/views.py
+++ server/cabinet/views.py
@@ -518,28 +518,15 @@ def change_group_view(request, group_id):
                 #     group_obj.participants.clear()
                 actions_group = group_obj.actions.all()
                 participants = group_obj.participants.all()
-                group_actions = GroupAction.objects.all()
                 users = []
-                actions_output = []
+                actions = []
+                for action in actions_group:
+                    actions.append(action.action + ' ' + str(action.num))
                 for profile in participants:
-                    users.append({'name': profile.first_name + ' ' + profile.last_name + ' ' + profile.middle_name,
-                                  'pk': profile.pk})
-                group_actions_output = []
-                for group_action in group_actions:
-                    actions_output = []
-                    for action in group_action.available_actions.all():
-                        if action in actions_group:
-                            actions_output.append({'pk': action.pk, 'name': action.action,
-                                                   'code': action.num, 'checked': True})
-                        else:
-                            actions_output.append({'pk': action.pk, 'name': action.action,
-                                                   'code': action.num, 'checked': False})
-                    group_actions_output.append(
-                        {'pk': group_action.pk, 'name': group_action.name, 'actions': actions_output})
-
+                    users.append({'name': profile.first_name + ' ' + profile.last_name + ' ' + profile.middle_name})
                 fields = {'name': group_obj.name, 'description': group_obj.description,
-                          'users': users, 'groups_actions': group_actions_output}
-                data = [{'pk': group_obj.pk, 'fields': fields}]
+                          'users': users, 'actions': actions}
+                data = {'pk': group_obj.pk, 'fields': fields}
                 return HttpResponse(json.dumps(data, ensure_ascii=False).encode("utf8"))
 
 
