e9f5c519259e69437f2920a540c8a6af33a3a8e7
Yura <ledovskij.yura@gmail.com>
2020-09-28 14:15:18 +0700

    Auf

--- server/cabinet/departaments_scripts.py
+++ server/cabinet/departaments_scripts.py
@@ -5,7 +5,7 @@ def get_salary_fields(user, month, year):
     salary_common, created_c = SalaryCommon.objects.get_or_create(date__month=month, date__year=year)
     salary, created_i = SalaryIndividual.objects.get_or_create(date__month=month, date__year=year, person=user)
     data = {'work_days': salary.days_worked, 'hours_worked': salary.time_from_report,
-            'time_norm': salary.time_norm, 'penalty': salary.penalty,
+            'time_norm_individual': salary.time_norm, 'penalty': salary.penalty,
             'is_penalty': salary.is_penalty,
             'time_off': salary.time_off, 'plan_salary': salary.plan_salary,
             'award': salary.award, 'salary_hand': salary.salary_hand,
@@ -58,43 +58,45 @@ def build_level_with_user(subdepartment_id, lvl, date, only_user=0, salary_flag=
                        'SRI_SAS': worker.SRI_SAS, 'pk': worker.pk}
         if only_user:
             continue
-        reports = Report.objects.filter(date__month=month, date__year=year, creator_id=worker.pk)
-        if reports:
-            users_field['has_report'] = True
-        else:
-            users_field['has_report'] = False
         report_time = 0
         flag = 0
-        for report in reports:
-            if report.status and flag != 2:
-                flag = 1
-            if flag == 1:
-                users_field[
-                    'banned'] = ' '.join([report.ban_id.first_name,
-                                          report.ban_id.last_name, report.ban_id.middle_name])
-                users_field['report_status'] = report.status
-                if report.check:
-                    users_field[
-                        'checker'] = ' '.join([report.check_id.first_name,
-                                               report.check_id.last_name, report.check_id.middle_name])
-                flag = 1
-            report_time += report.hour
-        if flag == 0:
-            users_field['banned'] = ''
-            users_field['checker'] = ''
-        times_cards = TimeCard.objects.filter(date__month=month, date__year=year, user=worker.user.pk)
-        time_system = 0
-        for time_card in times_cards:
-            time_system += time_card.hours_worked.hour
         salary = SalaryIndividual.objects.filter(date__month=month, date__year=year, person=worker)
-        users_field['time_report'] = report_time
-        if salary:
-            users_field['time_norm'] = salary[0].time_norm
-        else:
-            users_field['time_norm'] = 0
-        users_field['time_system'] = time_system
+        if salary_flag != 1:
+            reports = Report.objects.filter(date__month=month, date__year=year, creator_id=worker.pk)
+            if reports:
+                users_field['has_report'] = True
+            else:
+                users_field['has_report'] = False
+            for report in reports:
+                if report.status and flag != 2:
+                    flag = 1
+                if flag == 1:
+                    users_field[
+                        'banned'] = ' '.join([report.ban_id.first_name,
+                                              report.ban_id.last_name, report.ban_id.middle_name])
+                    users_field['report_status'] = report.status
+                    if report.check:
+                        users_field[
+                            'checker'] = ' '.join([report.check_id.first_name,
+                                                   report.check_id.last_name, report.check_id.middle_name])
+                    flag = 1
+                report_time += report.hour
+            if flag == 0:
+                users_field['banned'] = ''
+                users_field['checker'] = ''
+            times_cards = TimeCard.objects.filter(date__month=month, date__year=year, user=worker.user.pk)
+            time_system = 0
+            for time_card in times_cards:
+                time_system += time_card.hours_worked.hour
+            users_field['time_report'] = report_time
+            users_field['time_system'] = time_system
+            if salary:
+                users_field['time_norm'] = salary[0].time_norm
+            else:
+                users_field['time_norm'] = 0
         if salary and salary_flag == 1:
-            users_field['salary'] = get_salary_fields(worker, month, year)
+            users_field['position'] = worker.position
+            users_field.update(get_salary_fields(worker, month, year))
         users.append(users_field)
     data['users'] = users
     if subdepartments:
